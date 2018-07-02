2525f748218591229c65c9ef4b1b0b0c2c633e5c
Alexander Melo <alexandermelox@gmail.com>
2018-07-01 22:48:19 -0500

    added register and login component

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,13 +1,68 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Login extends Component {
+  state = {
+    email: '',
+    password: '',
+    errors: {}
+  }
+
+  onChangeHandler = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  onSubmitHandler = (event) => {
+    event.preventDefault();
+
+    const user = {
+      email: this.state.email,
+      password: this.state.password
+    };
+
+    console.log(user);
+  }
+
   render() {
     return (
-      <div>
-        <h1>Login</h1>
+      <div className="login">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Log In</h1>
+              <p className="lead text-center">Sign in to your DevConnector account</p>
+              <form onSubmit={this.onSubmitHandler}>
+                <div className="form-group">
+                  <input 
+                    type="email" 
+                    className="form-control form-control-lg" 
+                    placeholder="Email Address" 
+                    name="email"
+                    value={this.state.email}
+                    onChange={this.onChangeHandler} />
+                </div>
+                <div className="form-group">
+                  <input 
+                    type="password" 
+                    className="form-control form-control-lg" 
+                    placeholder="Password" 
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.onChangeHandler} />
+                </div>
+                <input type="submit" className="btn btn-info btn-block mt-4" />
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
+}
+
+Login.propTypes = {
+  email: PropTypes.string,
+  password: PropTypes.string
 }
 
 export default Login;
--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,13 +1,98 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import axios from 'axios';
 
 class Register extends Component {
+  state = {
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+    errors: {}
+  };
+
+  onChangeHandler = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  onSubmitHandler = (event) => {
+    event.preventDefault();
+
+    const newUser = {
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password,
+      password2: this.state.password2,
+    };
+
+    axios.post('/api/users/register', newUser)
+      .then(result => console.log(result))
+      .catch(error => console.log(error.response.data));
+  }
+
   render() {
     return (
-      <div>
-        <h1>Register</h1>
+      <div className="register">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Sign Up</h1>
+              <p className="lead text-center">Create your DevConnector account</p>
+              <form onSubmit={this.onSubmitHandler}>
+                <div className="form-group">
+                  <input 
+                    type="text" 
+                    className="form-control form-control-lg" 
+                    placeholder="Name" 
+                    name="name" 
+                    value={this.state.name} 
+                    onChange={this.onChangeHandler} />
+                </div>
+                <div className="form-group">
+                  <input 
+                    type="email" 
+                    className="form-control form-control-lg" 
+                    placeholder="Email Address" 
+                    name="email" 
+                    value={this.state.email}
+                    onChange={this.onChangeHandler} />
+                  <small className="form-text text-muted">This site uses Gravatar so if you want a profile image, use a Gravatar email</small>
+                </div>
+                <div className="form-group">
+                  <input 
+                    type="password" 
+                    className="form-control form-control-lg" 
+                    placeholder="Password" 
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.onChangeHandler} />
+                </div>
+                <div className="form-group">
+                  <input 
+                    type="password" 
+                    className="form-control form-control-lg" 
+                    placeholder="Confirm Password" 
+                    name="password2" 
+                    value={this.state.password2}
+                    onChange={this.onChangeHandler} />
+                </div>
+                <input type="submit" className="btn btn-info btn-block mt-4" />
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
+}
+
+Register.propTypes = {
+  name: PropTypes.string,
+  email: PropTypes.string,
+  password: PropTypes.string,
+  password2: PropTypes.string,
+  errors: PropTypes.object
 }
 
 export default Register;
