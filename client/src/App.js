e0bd1be65e45546cf71c74303de3a10fbe7215fc
Alexander Melo <alexander.melo@ibm.com>
2018-07-05 16:32:01 -0500

    changed something in the imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,28 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './store';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
 
-import Navbar from './components/Layout/Navbar';
-import Footer from './components/Layout/Footer';
-import Landing from './components/Layout/Landing';
-import Register from './components/Register/Register';
-import Login from './components/Login/Login';
+import Navbar from "./components/Layout/Navbar";
+import Footer from "./components/Layout/Footer";
+import Landing from "./components/Layout/Landing";
+import Register from "./components/Register/Register";
+import Login from "./components/Login/Login";
 
-import './App.css';
-
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <Provider store={ store }>
+      <Provider store={store}>
         <Router>
           <div className="App">
             <Navbar />
-              <Route exact path="/" component={ Landing } />
-              <div className="container">
-                <Route exact path='/register' component={ Register }/>
-                <Route exact path='/login' component={ Login }/>
-              </div>
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </div>
             <Footer />
           </div>
         </Router>
